5e66044cae036b6c4499bc26a838d33c338c6aeb
Alexandre Faustino <alexmigf@gmail.com>
2025-01-16 16:52:24 +0000

    Update DatiGeneraliHandler.php

--- ubl/Handlers/Body/DatiGeneraliHandler.php
+++ ubl/Handlers/Body/DatiGeneraliHandler.php
@@ -11,48 +11,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 class DatiGeneraliHandler extends UblHandler {
 
 	public function handle( $data, $options = array() ) {
-		// <DatiGenerali>
-		// 	<DatiGeneraliDocumento>
-		// 		<TipoDocumento>TD01</TipoDocumento>
-		// 		<Divisa>EUR</Divisa>
-		// 		<Data>2017-01-18</Data>
-		// 		<Numero>123</Numero>
-		// 	</DatiGeneraliDocumento>
-		// 	<DatiRicezione>
-		// 		<RiferimentoNumeroLinea>1</RiferimentoNumeroLinea>
-		// 		<IdDocumento>789</IdDocumento>
-		// 		<NumItem>5</NumItem>
-		// 		<CodiceCUP>123abc</CodiceCUP>
-		// 		<CodiceCIG>456def</CodiceCIG>
-		// 	</DatiRicezione>
-		// 	<DatiTrasporto>
-		// 		<DatiAnagraficiVettore>				
-		// 			<IdFiscaleIVA>
-		// 			<IdPaese>IT</IdPaese>
-		// 			<IdCodice>24681012141</IdCodice>
-		// 			</IdFiscaleIVA>
-		// 			<Anagrafica>
-		// 			<Denominazione>Trasporto spa</Denominazione>
-		// 			</Anagrafica>
-		// 		</DatiAnagraficiVettore>
-		// 		<DataOraConsegna>2017-01-10T16:46:12.000+02:00</DataOraConsegna>
-		// 	</DatiTrasporto>
-		// </DatiGenerali>
+		$divisa = $this->document->order->get_currency();
+		$data   = $this->document->order_document->get_date()->date_i18n( 'Y-m-d' );
+		$numero = ! empty( $this->document->order_document->get_number() ) ? $this->document->order_document->get_number()->get_formatted() : '';
 		
 		$datiGenerali = array(
 			'name'  => 'DatiGenerali',
 			'value' => array(
 				array(
 					'name'  => 'DatiGeneraliDocumento',
-					'value' => 'test',
-				),
-				array(
-					'name'  => 'DatiRicezione',
-					'value' => 'test',
-				),
-				array(
-					'name'  => 'DatiTrasporto',
-					'value' => 'test',
+					'value' => array(
+						array(
+							'name'  => 'TipoDocumento',
+							'value' => 'TD01',
+						),
+						array(
+							'name'  => 'Divisa',
+							'value' => $divisa,
+						),
+						array(
+							'name'  => 'Data',
+							'value' => $data,
+						),
+						array(
+							'name'  => 'Numero',
+							'value' => $numero,
+						),
+					),
 				),
 			),
 		);
