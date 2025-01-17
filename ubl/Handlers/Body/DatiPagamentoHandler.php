97603583e31e51877c12908af413d8eab7595c2c
Alexandre Faustino <alexmigf@gmail.com>
2025-01-17 09:50:07 +0000

    Wip

--- ubl/Handlers/Body/DatiPagamentoHandler.php
+++ ubl/Handlers/Body/DatiPagamentoHandler.php
@@ -10,26 +10,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class DatiPagamentoHandler extends UblHandler {
 
-	public function handle( $data, $options = array() ) {
-		// <DatiPagamento>
-		// 	<CondizioniPagamento>TP01</CondizioniPagamento>
-		// 	<DettaglioPagamento>
-		// 		<ModalitaPagamento>MP01</ModalitaPagamento>
-		// 		<DataScadenzaPagamento>2017-03-30</DataScadenzaPagamento>
-		// 		<ImportoPagamento>30.50</ImportoPagamento>
-		// 	</DettaglioPagamento>
-		// </DatiPagamento>
-	
+	public function handle( $data, $options = array() ) {	
 		$datiPagamento = array(
 			'name'  => 'DatiPagamento',
 			'value' => array(
 				array(
 					'name'  => 'CondizioniPagamento',
-					'value' => 'test',
+					'value' => 'TP01',
 				),
 				array(
 					'name'  => 'DettaglioPagamento',
-					'value' => 'test',
+					'value' => array(
+						array(
+							'name'  => 'ModalitaPagamento',
+							'value' => $this->get_modalita_pagamento_code(),
+						),
+						array(
+							'name'  => 'DataScadenzaPagamento',
+							'value' => $this->get_date_paid(),
+						),
+						array(
+							'name'  => 'ImportoPagamento',
+							'value' => $this->document->order->get_total(),
+						),
+					),
 				),
 			),
 		);
@@ -37,6 +41,42 @@ class DatiPagamentoHandler extends UblHandler {
 		$data[] = apply_filters( 'wpo_ips_fatturapa_handle_DatiPagamento', $datiPagamento, $data, $options, $this );
 
 		return $data;
+	}
+	
+	/**
+	 * Get the ModalitaPagamento code based on the payment method
+	 *
+	 * @return string
+	 */
+	private function get_modalita_pagamento_code(): string {
+		$paymentMethod = $this->document->order->get_payment_method();
+		
+		// Map WooCommerce payment methods to ModalitaPagamento codes
+		$mapping = apply_filters( 'wpo_ips_fatturapa_modalita_pagamento_code_mapping', array(
+			'cod'    => 'MP01',
+			'bacs'   => 'MP05',
+			'cheque' => 'MP07',
+			'paypal' => 'MP08',
+			'stripe' => 'MP08',
+		), $this );
+	
+		// Default to MP19 (Other) if no match is found
+		return isset( $mapping[ $paymentMethod ] ) ? $mapping[ $paymentMethod ] : 'MP19';
+	}
+	
+	/**
+	 * Get the date paid
+	 *
+	 * @return string
+	 */
+	private function get_date_paid(): string {
+		$order_date_paid = $this->document->order->get_date_paid();
+		
+		if ( ! $order_date_paid ) {
+			$order_date_paid = $this->document->order_document->get_date();
+		}
+		
+		return $order_date_paid->date_i18n( 'Y-m-d' );
 	}
 
 }
